70543153f4b8557d7bdb09f8d55e9f64830b2c62
Philippe Paré <phyyl@live.ca>
2021-04-23 21:20:53 -0400

    Added project structure

---
+++ LD48/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LD48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
